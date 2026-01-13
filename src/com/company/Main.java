6c3d7ec6fa041571e78a311f77abe95ffdc119f2
alfiya <alfiyaadilbek@gmail.com>
2026-01-13 21:14:58 +0500

    k

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import com.company.controllers.UserController;
+import com.company.controllers.interfaces.IUserController;
+import com.company.data.PostgresDB;
+import com.company.data.interfaces.IDB;
+import com.company.repositories.UserRepository;
+import com.company.repositories.interfaces.IUserRepository;
+
+public class Main {
+    public static void main(String[] args) {
+        IDB db = new PostgresDB("jdbc:postgresql://localhost:5432", "postgres", "0000", "somedb");
+        IUserRepository repo = new UserRepository(db);
+        IUserController controller = new UserController(repo);
+
+        MyApplication app = new MyApplication(controller);
+
+        app.start();
+
+        db.close();
+    }
+}
+
