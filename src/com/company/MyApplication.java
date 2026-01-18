404e0afee8a18536334d5553fd9ac6ccbccb39f4
eseng <esengulov070707@gmail.com>
2026-01-18 18:32:16 +0500

    Updated menu

--- src/com/company/MyApplication.java
+++ src/com/company/MyApplication.java
@@ -39,7 +39,7 @@ public class MyApplication {
         System.out.println("1. View all cars");
         System.out.println("2. Get car by ID");
         System.out.println("3. Filter cars");
-        System.out.println("0. Exit");
+        System.out.println("0. Back");
         System.out.print("Choose option: ");
     }
 
@@ -48,7 +48,7 @@ public class MyApplication {
         System.out.println("1. View all cars");
         System.out.println("2. Get car by ID");
         System.out.println("3. Create car");
-        System.out.println("0. Logout");
+        System.out.println("0. Back");
         System.out.print("Choose option: ");
     }
 
@@ -68,18 +68,25 @@ public class MyApplication {
     }
 
 
-    private void handleUserOption(int option) {
-        switch (option) {
-            case 1 -> getAllCarsMenu();
-            case 2 -> getCarByIdMenu();
-            case 3-> FilterCars();
-            case 0 -> {
-                System.out.println("Goodbye!");
-                System.exit(0);
+    private void runUserMenu() {
+        while (true) {
+            userMenu();
+            int option = scanner.nextInt();
+
+            switch (option) {
+                case 1 -> getAllCarsMenu();
+                case 2 -> getCarByIdMenu();
+                case 3 -> FilterCars();
+                case 0 -> {
+                    System.out.println("Goodbye!");
+                    return;
+                }
+                default -> System.out.println("Invalid option");
             }
-            default -> System.out.println("Invalid option");
         }
     }
+
+
 
     private void handleAdminOption(int option) {
         switch (option) {
@@ -103,8 +110,7 @@ public class MyApplication {
 
                     switch (choice) {
                         case 1 -> {
-                            userMenu();
-                            handleUserOption(scanner.nextInt());
+                            runUserMenu();
                         }
                         case 2 -> adminLogin();
                         case 0 -> {
